7af5c707633d15e2bf3b89d21a4bb1ac3b6dea50
Noah Lemen <noah.lemen@refinery29.com>
2017-06-09 12:51:07 -0400

    lazy w4ex1.2 solution

--- 04/hw.hs
+++ 04/hw.hs
@@ -8,3 +8,15 @@ fun1 (x:xs)
 
 fun1' :: [Integer] -> Integer
 fun1' = product . map ((-) 2) . filter even
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n | even n = n + fun2 (n `div` 2)
+       | otherwise = fun2 (3 * n + 1)
+
+fun2' :: Integer -> Integer
+fun2' = sum . filter even . takeWhile (> 1) . iterate operate
+
+operate :: Integer -> Integer
+operate n | even n = (n `div` 2)
+          | otherwise = (3 * n + 1)
