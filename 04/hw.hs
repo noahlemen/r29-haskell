8de39ad7a9abff7bd96f8ba9980ef2d63b051407
Noah Lemen <noah.lemen@refinery29.com>
2017-06-09 12:27:39 -0400

    w04ex1.1

---
+++ 04/hw.hs
@@ -0,0 +1,10 @@
+module Hw4 where
+
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+  | even x = (x - 2) * fun1 xs
+  | otherwise = fun1 xs
+
+fun1' :: [Integer] -> Integer
+fun1' = product . map ((-) 2) . filter even
